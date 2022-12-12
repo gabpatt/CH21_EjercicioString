cc8be3ed62319225e1cec624edb47e1005d9e660
gabpattt <gcortespatino@gmail.com>
2022-12-12 17:40:29 -0600

    ejercicio 3 validación completo

--- validación/js/main.js
+++ validación/js/main.js
@@ -1,16 +1,25 @@
-let btnSend = document.getElementById('sendBtn');
+//Ejercicios Strings
+let btnenviar = document.getElementById("btnEnviar");
 
-btnSend.addEventListener('click', function (e) {
-  e.preventDefault();
-  let inputMail = document.getElementById('exampleInputEmail1');
-  //   let inputPass = document.getElementById('exampleInputPassword1');
-  let alert = document.getElementById('alert');
-  alert.style.display = 'none';
-  console.log(inputMail.value.trim().replaceAll('  ', ''));
-  if (inputMail.value.trim().replaceAll('  ', '').length < 20) {
-    alert.innerHTML = 'DEBEN SER MAS DE 20 CARACTERES';
-    alert.style.display = 'block';
-    inputMail.focus();
-    inputMail.select();
-  }
+btnenviar.addEventListener("click", function(event){
+    event.preventDefault();
+    let email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let exampleFormControlInput1 = document.getElementById("exampleFormControlInput1");
+    let exampleFormControlTextarea1 = document.getElementById("exampleFormControlTextarea1");
+    let alertError = document.getElementById("alertError");
+    exampleFormControlTextarea1.value = exampleFormControlTextarea1.value.trim();
+    alertError.style.display="none";
+    alertError.innerHTML="";
+
+    if (exampleFormControlTextarea1.value.trim().replaceAll("  ","").length < 20){
+        alertError.innerHTML = "El mensaje debe contener 20 caracteres o mas";
+        alertError.style.display="block";
+        exampleFormControlTextarea1.focus();
+        exampleFormControlTextarea1.select();
+    }
+
+    if (exampleFormControlInput1.value.match(email) == null){
+        alertError.style.display="block";
+        alertError.innerHTML += "<br/>El correo electronico no es valido.";
+    }
 });
