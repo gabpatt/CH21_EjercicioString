f872c650ea7b8d58251bdeb75123fcadec203fef
gabpattt <gcortespatino@gmail.com>
2022-12-12 16:11:56 -0600

    Se agragó Palindrome

--- ejercicio 2/js/main.js
+++ ejercicio 2/js/main.js
@@ -8,3 +8,22 @@ function invertido(){
     return nuevo;
 }
 console.log(invertido ("Laura Aline"));
+
+
+
+//Palindromo: palabra o frase que se lee igual de izq a derecha que de derecha a izq
+// amma, Otto, Anina, Natan, Siris, Ana, Ada, Aziza, Ramar, Nayan
+
+
+
+function esPalindro(str) {
+    let newStr = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+      newStr += str[i];
+    }
+  
+    return str.toLowerCase().split(' ').join('') ==
+      newStr.toLocaleLowerCase().split(' ').join('')
+      ? `${str} es un palindromo`
+      : `${str} no es un palindromo`;
+  }
