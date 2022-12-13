ebfc54f311de7fb547137245e578e9c4b79f14e1
gabpattt <gcortespatino@gmail.com>
2022-12-13 12:24:44 -0600

    Se agregó validación de RFC

--- validación/js/main.js
+++ validación/js/main.js
@@ -3,10 +3,12 @@ let btnenviar = document.getElementById("btnEnviar");
 
 btnenviar.addEventListener("click", function(event){
     event.preventDefault();
-    let email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     let exampleFormControlInput1 = document.getElementById("exampleFormControlInput1");
     let exampleFormControlTextarea1 = document.getElementById("exampleFormControlTextarea1");
     let alertError = document.getElementById("alertError");
+    let exampleFormControlInput2 = document.getElementById("exampleFormControl2");
+    let validos= "";
     exampleFormControlTextarea1.value = exampleFormControlTextarea1.value.trim();
     alertError.style.display="none";
     alertError.innerHTML="";
@@ -16,10 +18,48 @@ btnenviar.addEventListener("click", function(event){
         alertError.style.display="block";
         exampleFormControlTextarea1.focus();
         exampleFormControlTextarea1.select();
+        exampleFormControlTextarea1.style.border ="solid red 1px";
+    }
+    else{
+        exampleFormControlTextarea1.style.border ="solid green 1px";
+        validos++;
     }
 
-    if (exampleFormControlInput1.value.match(email) == null){
+    if (exampleFormControlInput1.value.match(emailRegex) == null){
         alertError.style.display="block";
-        alertError.innerHTML += "<br/>El correo electronico no es valido.";
+        alertError.innerHTML += "<br/>El correo electronico no es válido.";
+        exampleFormControlInput1.style.border ="solid red 1px";
     }
+    else{
+        exampleFormControlInput1.style.border ="solid green 1px";
+        validos++;
+    }
+
+
+    let RFCRegex = /^[A-Z]{4}[0-9]{6}[A-Z0-9]{3}$/;
+    exampleFormControlInput2.value = exampleFormControlInput2.value.toUpperCase();
+    if (exampleFormControlInput2.value.match(RFCRegex) == null){
+        alertError.style.display="block";
+        alertError.innerHTML += "<br/>El RFC no es válido.";
+        exampleFormControlInput2.style.border ="solid red 1px";
+    }
+    else{
+        exampleFormControlInput2.style.border ="solid green 1px";
+        validos++;
+    }
+    if (idTimeout != undefined && idTimeout != null) {
+        clearTimeout(idTimeout);
+    }
+    
+
+
+    if (validos === 3){
+        idTimeout = setTimeout (function(){
+            exampleFormControlTextarea1.style.border ="";
+            exampleFormControlInput1.style.border="";
+            exampleFormControlInput2.style.border="";
+        }, 3000);
+        console.log(idTimeout);
+    }
+
 });
